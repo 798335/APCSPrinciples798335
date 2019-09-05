ee29099ce90baa3dd43d20910aefea6026d09731
Victoria Melamed <victoriamelamed@MacBook-Pro.local>
2019-09-04 19:40:07 -0700

    rewrote code because there was a mistake

--- lab93/ball.js
+++ lab93/ball.js
@@ -1,72 +1,83 @@
 //Gabrielle Melmamed
 //August 28, 2019
 
-class Ball{
+class Ball {
   constructor(x, y, dx, dy, id){
-    this.loc = createVector(x,y);
-    this.vel = createVector(dx,dy);
-    this.acc = createVector(0,1);
-    this.clr = color(random(255), random(255), random(255));
-    this.id = id;
-    this.w=15
-    if(this.id<0){
-      this.w=50
-    }
+   this.loc = createVector(x, y);
+   this.vel = createVector(dx, dy);
+   this.acc = createVector(0,0);
+   this.clr = color(random(255), random(255), random(255));
+   this.id = id;
 
   }
 
   run(){
-    this.checkEdges();
+    this.checkedges();
     this.update();
     this.render();
   }
 
-  checkEdges(){
+  checkedges(){
     if(this.loc.x < 0){
       this.vel.x = -this.vel.x;
     }
     if(this.loc.x > width){
       this.vel.x = -this.vel.x;
     }
-
     if(this.loc.y < 0){
       this.vel.y = -this.vel.y;
     }
-
     if(this.loc.y > height){
       this.vel.y = -this.vel.y;
-      this.loc.y = height - 2;
+      this.loc.y = height -2;
     }
+
   }
-
   update(){
-    //this.clr = color(random(255), random(255), random(255));
-    //this.x = this.x + this.dx;
-    //this.y = this.y + this.dy;
-    this.vel.add(this.acc);
-    this.loc.add(this.vel);
     var distToMainBall;
-    if(this.id >= 0){
-      distToMainBall = this.loc.dist(mainBall.loc);
-      if(distToMainBall < 250){
-        this.acc = p5.Vector.sub(mainBall.loc, this.loc);
-        this.acc.normalize();
-        this.acc.mult(0,1);
-        }
-        if(distToMainBall < 150){
-          this.acc = p5.Vector.sub(this.loc, mainBall.loc);
-          this.acc.normalize();
-          this.acc.mult(0.5);
-        }
+    var distToMainBall2;
+    var distToMidBall
+//    var distToMid;
+    if(this.id > 2){
+     distToMainBall = this.loc.dist(mainBall.loc);
+     distToMainBall2 = this.loc.dist(mainBall2.loc);
 
+     if(distToMainBall < 250){
+       //add attraction
+       this.acc = p5.Vector.sub(mainBall.loc, this.loc);
+       this.acc.normalize();
+       this.acc.mult(0.1);
+     }
+     if(distToMainBall < 150){ // add repulsion
+       this.acc = p5.Vector.sub(this.loc, mainBall.loc);
+       this.acc.normalize();
+       this.acc.mult(0.5);
+     }
+     if(distToMainBall2 < 250){
+       //add attraction
+       this.acc = p5.Vector.sub(mainBall2.loc, this.loc);
+       this.acc.normalize();
+       this.acc.mult(0.1);
+     }
+     if(distToMainBall2 < 150){ // add repulsion
+       this.acc = p5.Vector.sub(this.loc, mainBall2.loc);
+       this.acc.normalize();
+       this.acc.mult(0.5);
+     }
     }
+    this.vel.limit(5);
+     this.vel.add(this.acc);
+   this.loc.add(this.vel);
+
+
+  }
 
   render(){
     fill(this.clr);
-    ellipse(this.loc.x, this.loc.y, this.w, this.w);
-    //fill(this.clr);
-    //rect(this.x, this.y, this.width, this.width);
-    //fill(this.clr);
-    //triangle(this.x, this.y, 50, 50, 100, 100);
+    if (this.id <3 ){
+      ellipse (this.loc.x, this.loc.y, 40, 40);
+    }else {
+    ellipse(this.loc.x, this.loc.y, 15, 15);
   }
-}// end ball class
+  }
+}//  +++++++++++++++++++++++++++++++++++  End Ball Class
--- lab93/sketch.js
+++ lab93/sketch.js
@@ -2,42 +2,38 @@
 // Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
-var balls = [];
+var balls = []; //declares array
 var mainBall;
+var mainBall2;
+var midBall;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(5, 5, 5, 5);
-  fill(200, 30, 150);
-  //b1 = new Ball(random(width), random(height), random(-5,5), random(-5,5));
-  // b2 = new Ball(random(width), random(height), random(-5,5), random(-5,5));
-  //b3 = new Ball(random(width), random(height), random(-5,5), random(-5,5));
-  //b4 = new Ball(random(width), random(height), random(-5,5), random(-5,5));
-  loadBalls(50);
+  background(5, 5, 5);
+
+  loadBalls(70);
+
+}
 
 
- }
+function draw() {
+background(5,5,5,20);
+midBall = ellipse (400, 400, 60, 60 );
+  runBalls();
+}
 
- //  The draw function is called @ 30 fps
- function draw() {
-  //b1.run();
-  //b2.run();
-  //b3.run();
-  //b4.run();
-   background(5, 5, 5, 5);
-   runBalls();
-   }
+function loadBalls(x){
+  mainBall = new Ball(random(width/2), random(height/2), random (-.4,.4), random(-.4,.4), 1);
+  mainBall2 = new Ball(random(width/2), random(height/2), random (-.4,.4), random(-.4,.4), 2);
+  for(var i = 0; i < x; i++){
+    balls[i]=new Ball(random(width), random(height), random (-1,1), random(-1,1), i+3);
+  }
+}
 
-  function loadBalls(n){
-    mainBall = new Ball(width/s, height/2, random(-1,1), random(-1,1), -1);
-     for(var i = 0; i < n; i++){
-       balls[i] = new Ball(random(0, 800), random(0, 800), random(-8,8), random(-8,8), i);
-     }
-   }
 function runBalls(){
   mainBall.run();
+  mainBall2.run();
   for(var i = 0; i < balls.length; i++){
     balls[i].run();
-
-  }
+}
 }
