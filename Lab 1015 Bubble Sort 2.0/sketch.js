428f787d36a51125890908c1bbbf0af26d5c039e
Victoria Melamed <victoriamelamed@MacBook-Pro.local>
2019-10-16 16:55:18 -0700

    added a function to bubble sort that creates a random array

--- Lab 1015 Bubble Sort 2.0/sketch.js
+++ Lab 1015 Bubble Sort 2.0/sketch.js
@@ -3,20 +3,33 @@
 //  This is a comment
 //  The setup function function is called once when your program begins
 
-var list = [3, 5, 4, 9, 7, 1, 81, 53, 49, 75];//creates the array
-console.log(list);
+var l = [];
+function loadList(n){
+  for(var i = 0; i < n; i++){//keeps adding numbers onto the array until i < n, the number of items in the array
+    l.push(int(random(0, 10)));//pushes a random integer between 0 and 1000 into the array
+  }
+}//function generates a random array with as many numbers as the input is
+
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+  loadList(10);
+  console.log(l);
+  for(var i = 0; i < l.length; i++){
+    for(var j = 0; j < l.length; j++){
+      if(l[j] > l[j + 1]){
+        swap(l, j, j + 1);
+        }
+    }
+  }
+  console.log(l);
+}
+
 
 function swap(list, a, b) {
   var temp = list[a];
   list[a] = list[b];
   list[b] = temp;
 }
-
-for(var i = 0; i < list.length; i++){
-  for(var j = 0; j < list.length; j++){
-    if(list[j] > list[j + 1]){
-      swap(list, j, j + 1);
-      }
-  }
-}
-console.log(list);
