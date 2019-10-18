77918a97bd3b7f3237b5071bc3c43769823f0462
798335 <54591145+798335@users.noreply.github.com>
2019-10-18 08:48:19 -0700

    working on selection sort

--- Lab 1018 Selection Sort/sketch.js
+++ Lab 1018 Selection Sort/sketch.js
@@ -2,11 +2,23 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
+var list = [];
+var temp;
+
+function loadList(n){
+  for(var i = 0; i < n; i++){//keeps adding numbers onto the array until i < n, the number of items in the array
+    list.push(int(random(0, 1000)));//pushes a random integer between 0 and 1000 into the array
+  }
+}//function generates a random array with as many numbers as the input is
+
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
+  loadList(100);
+  console.log(list);
+  //add sortihng code//
 
 
 }
