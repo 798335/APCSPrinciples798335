0815f2fe2c12cfb2e8c6fd87a0e9a05b93abb4c4
Victoria Melamed <victoriamelamed@MacBook-Pro.local>
2019-10-17 20:20:18 -0700

    finished Lab 1017

--- Lab 1017 Insertion Sort/__p5 StartTemplate/sketch.js
+++ Lab 1017 Insertion Sort/__p5 StartTemplate/sketch.js
@@ -7,7 +7,7 @@ var list = [];
 var temp;
 function loadList(n){
   for(var i = 0; i < n; i++){//keeps adding numbers onto the array until i < n, the number of items in the array
-    list.push(int(random(0, 10)));//pushes a random integer between 0 and 1000 into the array
+    list.push(int(random(0, 1000)));//pushes a random integer between 0 and 1000 into the array
   }
 }//function generates a random array with as many numbers as the input is
 
@@ -20,13 +20,27 @@ function setup() {
   loadList(10000);
   console.log(list);
   for(var i = 1; i < list.length; i++){
-    for(var j = 1; j > 0; j--){
+    for(var j = list.length; j > 0; j--){
       if(list[j] < list[j - 1]){
         swap(list, j, j - 1);
       }
     }
   }
-console.log(list);
+  console.log(list);
+  var evenodd = list.length % 2;
+  var halflengtheven = 0.5 * list.length;
+  var halflengthodd = (0.5 * list.length) - 0.5;
+  //console.log(halflengthodd);
+  var median = 0;
+  if(evenodd === 0){
+    median = (list[halflengtheven] + list[halflengtheven - 1])/2
+    console.log('median: ' + median);
+  }else{
+    median = list[halflengthodd];
+    console.log('median: ' + median)
+  }
+//var median = (list[5000] + list[5001])/2
+//console.log('median: ' + median);
 }
 //console.log(list);
 
