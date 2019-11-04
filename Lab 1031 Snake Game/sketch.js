cd02f5df4cd0255a872ae823785588a0ceb567c7
798335 <54591145+798335@users.noreply.github.com>
2019-11-04 08:50:08 -0800

    SNAKE HEAD MOVES!!!!!!!!

--- Lab 1031 Snake Game/sketch.js
+++ Lab 1031 Snake Game/sketch.js
@@ -2,17 +2,26 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
+var snake = [];
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
-  fill(200, 30, 150);
-
-
+  fill(114, 100, 100);
+  frameRate(10);
+  loadSnakes(1);
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
-  snake = new Snake(100, 100);
-  snake.run();
+  background(5, 5, 5);
+  for(var i = 0; i < 1; i++) {
+    snake[i].run();
+  }
+}
+
+function loadSnakes(n) {
+  for(var i = 0; i < n; i++) {
+    snake[i] = new Snake(20, 20, 15, 15);
+  }
 }
--- Lab 1031 Snake Game/snake.js
+++ Lab 1031 Snake Game/snake.js
@@ -1,34 +1,46 @@
 class Snake {
-
-  constuctor(x, y){
+  constructor(x, y, w, h) {
     this.loc = createVector(x, y);
-    this.clr = color(255, 105, 180);
-
+    this.w = w;
+    this.h = h;
+    //this.clr = color(255, 0, 0);
   }
 
   run() {
-    this.render();
     this.update();
-  }
-
-  render() {
-    fill(this.clr);
-    rect(this.loc.x, this.loc.y, 15, 15);
+    this.render();
   }
 
   update() {
+    this.keyReleased();
+  }
 
+  render() {
+    fill(255, 0, 0);
+    rect(this.loc.x, this.loc.y, this.w, this.h);
   }
 
 keyReleased() {
   if(keyCode === UP_ARROW) {
     this.loc.y = this.loc.y - this.h;
   }
-}
+  if(keyCode === DOWN_ARROW) {
+    this.loc.y = this.loc.y + this.h;
+    }
+  if(keyCode === LEFT_ARROW) {
+    this.loc.x = this.loc.x - this.w;
+  }
+if(keyCode === RIGHT_ARROW) {
+  this.loc.x = this.loc.x + this.w;
+  }
+  }
 
-  //keyPressed() {
-    //if(keyCode === UP_ARROW) {
-      //this.loc.add(this.loc.x+15)
-    //}
-  //}
+  checkEdges() {
+    if(this.loc.x > 800 ||
+      this.loc.x < 0 ||
+      this.loc.y > 800 ||
+      this.loc.y < 0){
+        gameState = 3;
+      }
+  }
 }
