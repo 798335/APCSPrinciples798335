c1395d00c4619a8b87b1bc58a11b624728ca4fcd
Victoria Melamed <victoriamelamed@MacBook-Pro.local>
2019-11-04 22:24:39 -0800

    working on snake game

--- Lab 1031 Snake Game/sketch.js
+++ Lab 1031 Snake Game/sketch.js
@@ -3,6 +3,7 @@
 //  This is a comment
 //  The setup function function is called once when your program begins
 var snake = [];
+var gameState = 1;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
@@ -15,6 +16,15 @@ function setup() {
 //  The draw function is called @ 30 fps
 function draw() {
   background(5, 5, 5);
+  if(gameState === 1){
+    startGame();
+  }else if(gameState === 2){
+    gameMode();
+  }else if(gameState === 3){
+    endGame();
+  }else if(gameState === 4){
+    instructions();
+  }
   for(var i = 0; i < 1; i++) {
     snake[i].run();
   }
@@ -24,4 +34,12 @@ function loadSnakes(n) {
   for(var i = 0; i < n; i++) {
     snake[i] = new Snake(20, 20, 15, 15);
   }
+}
+
+function startGame() {
+  clear();
+  background(5, 5, 5);
+  textSize(90);
+  fill(255, 255, 255);
+  text('The Snake Game', 30, 200);
 }
